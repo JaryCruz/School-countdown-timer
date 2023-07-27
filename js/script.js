24054f1d76042c47b049ace678729bf799911558
Jary Cruz <jarycruz@Sergios-MacBook-Pro.local>
2023-07-27 10:17:45 -0400

    Fixed Date

--- js/script.js
+++ js/script.js
@@ -3,21 +3,22 @@ const hoursElement = document.querySelector('[data-hours]')
 const minutesElement = document.querySelector('[data-minutes]')
 const secondsElement = document.querySelector('[data-seconds]')
 
-const finalDate = new Date('May 26, 2023 00:00:00').getTime()
+const finalDate = new Date('May 24, 2024 00:00:00').getTime()
 
 const timer = () => {
-  const now = new Date().getTime()
-  let diff = finalDate - now
-  // Showing An Alert When Counter Time Finishes
-  if (diff < 0) {
-    alert('School Is Ending')
+  const now = Date.now()
+  let waitingTime = finalDate - now
+
+  // Show An Alert When Counter Time Finishes
+  if (waitingTime < 0) {
+    alert('High School has ended')
   }
 
   // Calculate Time Left
-  let days = Math.floor(diff / (1000*60*60*24))
-  let hours = Math.floor(diff % (1000*60*60*24) / (1000*60*60))
-  let minutes = Math.floor(diff % (1000*60*60) / (1000*60))
-  let seconds = Math.floor(diff % (1000*60) / 1000)
+  let days = Math.floor(waitingTime / (1000 * 60 * 60 * 24))
+  let hours = Math.floor(waitingTime % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+  let minutes = Math.floor(waitingTime % (1000 * 60 * 60) / (1000 * 60))
+  let seconds = Math.floor(waitingTime % (1000 * 60) / 1000)
   
   // Add Zeros If Less Than 10
   days <= 9 ? days = `0${days}` : days
