e81b63b4875f349924624adbf2a873499d0fc209
Jary Cruz <jarycruz@proton.me>
2024-03-16 07:57:48 -0400

    Finished updating project

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-const daysElement = document.querySelector('[data-days]')
-const hoursElement = document.querySelector('[data-hours]')
-const minutesElement = document.querySelector('[data-minutes]')
-const secondsElement = document.querySelector('[data-seconds]')
+const daysElement = document.querySelector("[data-days]")
+const hoursElement = document.querySelector("[data-hours]")
+const minutesElement = document.querySelector("[data-minutes]")
+const secondsElement = document.querySelector("[data-seconds]")
 
-const finalDate = new Date('May 24, 2024 00:00:00').getTime()
+const finalDate = new Date("May 23, 2024 00:00:00").getTime()
 
 const timer = () => {
   const now = Date.now()
@@ -11,20 +11,22 @@ const timer = () => {
 
   // Show An Alert When Counter Time Finishes
   if (waitingTime < 0) {
-    alert('High School has ended')
+    alert("High School has ended")
   }
 
   // Calculate Time Left
   let days = Math.floor(waitingTime / (1000 * 60 * 60 * 24))
-  let hours = Math.floor(waitingTime % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
-  let minutes = Math.floor(waitingTime % (1000 * 60 * 60) / (1000 * 60))
-  let seconds = Math.floor(waitingTime % (1000 * 60) / 1000)
-  
+  let hours = Math.floor(
+    (waitingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  )
+  let minutes = Math.floor((waitingTime % (1000 * 60 * 60)) / (1000 * 60))
+  let seconds = Math.floor((waitingTime % (1000 * 60)) / 1000)
+
   // Add Zeros If Less Than 10
-  days <= 9 ? days = `0${days}` : days
-  hours <= 9 ? hours = `0${hours}` : hours
-  minutes <= 9 ? minutes = `0${minutes}` : minutes
-  seconds <= 9 ? seconds = `0${seconds}` : seconds
+  days <= 9 ? (days = `0${days}`) : days
+  hours <= 9 ? (hours = `0${hours}`) : hours
+  minutes <= 9 ? (minutes = `0${minutes}`) : minutes
+  seconds <= 9 ? (seconds = `0${seconds}`) : seconds
 
   daysElement.innerText = days
   hoursElement.innerText = hours
